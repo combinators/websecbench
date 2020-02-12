7fe1f76f1e08c72cbf11136ff3178c99fd98f201
JanBessai <jan.bessai@tu-dortmund.de>
2020-02-12 10:57:00 +0100

    Generate all brenchmarks into single branch

--- src/main/scala/org/combinators/websecbench/InhabitationController.scala
+++ src/main/scala/org/combinators/websecbench/InhabitationController.scala
@@ -8,13 +8,17 @@ import org.combinators.cls.types.Type
 import org.combinators.jgitserv.{BranchTransaction, GitService, ResourcePersistable}
 import org.combinators.templating.persistable.BundledResource
 import org.combinators.websecbench.SemanticTypes.JavaVoid
+import org.eclipse.jgit.lib.BranchConfig
 
-
-class BenchmarkController(
+case class BenchmarkSelector(
   tags: Set[ComponentTag],
   targetType: Type,
+  maximalNumberOfResults: Int
+)
+
+class BenchmarkController(
+  selectedBenchmarks: Set[BenchmarkSelector],
   benchmarkName: String,
-  maximalNumberOfResults: Int = 100,
   port: Int = 9000) extends IOApp {
 
   lazy val buildDotSbt: BundledResource =
@@ -24,54 +28,67 @@ class BenchmarkController(
       Paths.get("src", "main", "java", "org", "owasp", "benchmark", "helpers", "Utils.java"),
       getClass)
   lazy val storeResource = ResourcePersistable.apply
-  lazy val storeCompilationUnit = CodeGenerator.compilationUnitPersistable[Expression](benchmarkName)
-  lazy val storeVulnerabilityReport = CodeGenerator.vulnerabilityReportPersistable[Expression](benchmarkName)
-
-
-  lazy val Gamma = Repository.repository(tags)
-  lazy val results =
-    Gamma.inhabit[CodeGenerator[Expression]](targetType)
-
-  lazy val emptyBenchmarkBranch: String = "empty_benchmark"
+  
   lazy val emptyBenchmark: BranchTransaction =
     BranchTransaction
-      .empty(emptyBenchmarkBranch)
+      .empty(benchmarkName)
       .persist(buildDotSbt)(storeResource)
       .persist(owaspUtils)(storeResource)
       .commit("Add shared resources")
 
-  def transactionForResult(resultNumber: Int): BranchTransaction = {
-    val result = results.interpretedTerms.index(BigInt(resultNumber))
-    BranchTransaction
-      .checkout(emptyBenchmarkBranch)
-      .fork(s"variation_${resultNumber}")
-      .persist(result)(storeCompilationUnit)
-      .persist(result)(storeVulnerabilityReport)
-      .commit(s"Add benchmark ${benchmarkName} variation ${resultNumber}")
+  lazy val numberFormat: String = {
+    val maxBenchmarks = selectedBenchmarks.map(_.maximalNumberOfResults).sum
+    s"%0${maxBenchmarks.toString.length}d"    
   }
 
-  def transactions: Seq[BranchTransaction] =
+  def transactionFor(benchmarkSelector: BenchmarkSelector): Seq[Int => BranchTransaction] = { 
+    val Gamma = Repository.repository(benchmarkSelector.tags)
+    val results = Gamma.inhabit[CodeGenerator[Expression]](benchmarkSelector.targetType)
+    val toStore = results.size.map(s => 
+          Math.min(benchmarkSelector.maximalNumberOfResults, s.toInt)
+        ).getOrElse(benchmarkSelector.maximalNumberOfResults)
+
+    (0 until toStore).foldLeft(Seq.empty[Int => BranchTransaction]) { case (transactions, resultNumber) => 
+      val nextTransaction = (nextNumber : Int) => {
+        val currentName = s"%s_$numberFormat".format(benchmarkName, nextNumber)
+        val storeCompilationUnit = CodeGenerator.compilationUnitPersistable[Expression](currentName)
+        val storeVulnerabilityReport = CodeGenerator.vulnerabilityReportPersistable[Expression](currentName)
+        val result = results.interpretedTerms.index(BigInt(resultNumber))
+        BranchTransaction
+          .checkout(benchmarkName)
+          .persist(result)(storeCompilationUnit)
+          .persist(result)(storeVulnerabilityReport)
+          .commit(s"Add benchmark ${currentName}")
+      }
+      nextTransaction +: transactions
+    }
+  }
+  
+  def computeTransactions: Seq[BranchTransaction] =
     emptyBenchmark +:
-    Seq.tabulate[BranchTransaction](
-      results.size
-        .map(n => if (n > maximalNumberOfResults) maximalNumberOfResults else n.toInt)
-        .getOrElse(maximalNumberOfResults)
-    )(transactionForResult)
+    scala.util.Random.shuffle(selectedBenchmarks.toSeq.flatMap(transactionFor))
+      .zipWithIndex
+      .map { case (transaction, number) => transaction(number) }
 
   def run(args: List[String]): IO[ExitCode] = {
     for {
       _ <- IO { println(s"Computing solutions") }
-      branches = transactions
+      transactions = computeTransactions
       _ <- IO { println(s"Use: git clone http://127.0.0.1:${port}/$benchmarkName $benchmarkName") }
-      exitCode <- new GitService(branches, benchmarkName, port).run(args)
+      exitCode <- new GitService(transactions, benchmarkName, port).run(args)
     } yield exitCode
   }
 }
 
 object Benchmark42
   extends BenchmarkController(
-    tags = Set(ComponentTag.FileIO, ComponentTag.Process, ComponentTag.ReadFromRequest),
-    targetType = JavaVoid,
+    Set(
+      BenchmarkSelector(
+        tags = Set(ComponentTag.FileIO, ComponentTag.Process, ComponentTag.ReadFromRequest),
+        targetType = JavaVoid,
+        maximalNumberOfResults = 100
+      )
+    ),
     benchmarkName = "benchmark42"
   )
 
